8c467a523c6b77b2864dffdfb5eed2849c57549c
Tristan Schuler <tkschuler@yahoo.com>
2018-10-31 21:56:05 -0400

    Client now closes when Ctrl+c

--- client.py
+++ client.py
@@ -3,20 +3,27 @@
 import socket
 import fcntl, os
 import time
+import sys
 
-TCP_IP = '10.151.131.32'
-TCP_PORT = 5005
+TCP_IP = '127.0.0.1'
+#TCP_IP = '10.151.131.32'
+#TCP_IP = 'aerohab.eduroam.gmu.edu'
+
+TCP_PORT = 50010
 BUFFER_SIZE = 1024
-MESSAGE = "Hello, World!"
+MESSAGE = "stabilization"
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
+
 while 1:
-    s.send(MESSAGE)
-    data = s.recv(BUFFER_SIZE)
-    #s.close()
-
-    print "received data:", data
-    time.sleep(1)
-
-s.close()
+    try:
+        s.send(MESSAGE)
+        data = s.recv(BUFFER_SIZE)
+        print "received data:", data
+        time.sleep(1)
+    except KeyboardInterrupt:
+        print "Connection closed"
+        s.send("close")
+        s.close()
+        sys.exit()
